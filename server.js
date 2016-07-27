2c116b0e8554c10c6bde22981bc3c4fd503704d2
electricg <electric.g@gmail.com>
2016-07-27 21:58:29 +0100

    Added server files

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8085;
+
+// Handling routes.
+app.use(express.static(__dirname + '/'));
+
+// Run the server.
+var server = app.listen(port, function(){
+  console.log('Listening at http://%s:%s', server.address().address, server.address().port);
+});
